b0d43d094f168b7206a7c16ca11246a16e654663
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2025-02-18 19:10:58 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Токен, который вы получаете от BotFather
+TOKEN = '7612817210:AAE_RGYQX6kQU503S8gFWxEbmacHk9yb0PU'
+
+# ID администратора (ваш пользовательский ID)
+ADMIN_ID = 6916747393  # Замените на свой ID
+
+# Путь к файлу с фото для отправки пользователю
+PHOTO_PATH = 'login_screenshot.jpg'  # Путь к файлу изображения
